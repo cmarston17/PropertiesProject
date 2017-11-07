f934b44564c1dd45dc5cad4dd3a2bf712c64119e
CMarston17 <cmarston17@uc.apu.edu>
2017-11-07 04:46:37 -0800

    PropertiesProject
    using properties, get; set; ect.. keeping fields private. basics.

---
+++ PropertiesProject copy/PropertiesProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PropertiesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s = new Student("Peter Griffin", true, 20);
+            s.Age = 15;
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ PropertiesProject copy/PropertiesProject/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace PropertiesProject
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public bool Male { get; private set; }
+        public bool FullAged { get; private set; }
+        private int age;
+
+        /// <summary>
+        /// C# creates  get and set methods for us keeping the int age field priate and unmodifiable 
+        ///  and safe above. the field is lowercase and private where the below method is capitalized
+        /// </summary>
+        /// <value>The age.</value>
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+                // updating whether student is of age
+                FullAged = true;
+                if (age < 18)
+                    FullAged = false;
+            }
+        }
+        public Student(string name, bool gender, int age)
+        {
+            EditStudent(name, gender, age); 
+        }
+
+            public void EditStudent(string name, bool gender, int age)
+            {
+            Name = name;
+            Male = gender;
+            Age = age;
+            }
+
+        public override string ToString()
+        {
+            string iAmFullAged = "I'm";
+            if (!FullAged)
+                iAmFullAged = "I'm not";
+            string gender = "male";
+            if (!Male)
+                gender = "female";
+            return String.Format("I'm {0}, {1}. I'm {2} years old and {3} of age.", Name, gender, Age, iAmFullAged);
+        }
+
+
+    
+        
+       
+    }
+}
